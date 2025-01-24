bffd85801059d31aacd5f197ac560c87735dd565
Z-K11 <zulqarnainbazidkhail@gmail.com>
2025-01-24 22:16:29 +0500

    Calculate the activation function value for the first node in the hidden layers

--- main.py
+++ main.py
@@ -10,3 +10,10 @@ print(biases)
 x_1 =0.5
 x_2 =0.85
 print ('x1 is {} and x2 is {}'.format(x_1,x_2))
+z_11 = x_1*weights[0]+x_2*weights[1] + biases[0]
+z_12 = x_1*weights[2]+x_2*weights[3] + biases[1]
+print('the weighted sum of input at the first and second node of the hidden layer is {}  and {}'.format(np.around(
+    z_11,decimals=4),np.around(z_12,decimals=4)))
+'''assuming a sigmoid function as the activation function let's compute the activation of the first node '''
+a_11 = 1.0/(1.0+np.exp(-z_11))
+print('activation of the first node in the hidden layer is {}'.format(np.around(a_11,decimals=4)))
