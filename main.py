bb6f4cf6fce50b24380cf02c375a46fd90ecfc85
Z-K11 <zulqarnainbazidkhail@gmail.com>
2025-01-27 09:39:36 +0500

    create a function for forward propogation of neural network

--- main.py
+++ main.py
@@ -79,3 +79,20 @@ weighted_sum = compute_weighted_sum(inputs,node_weights,node_bias)
 print('The weighted sum at the first node in the hidden layer is {}'.format(np.around(weighted_sum[0], decimals=4)))
 activation = node_activation(weighted_sum)
 print('The output of the first node in the hidden layer is {}'.format(np.around(activation[0], decimals=4)))
+#forward propogation function
+def forward_propogation(network,inputs):
+    layer_inputs=list(inputs) # start with the input layer as the input to the first hidden layer 
+    for layer in network:
+        layer_data=network[layer]
+        layer_outputs=[]
+        for layer_node in layer_data:
+            node_data=layer_data[layer_node]
+            node_output = node_activation(compute_weighted_sum(layer_inputs,node_data['weights'],node_data['bias']))
+            layer_outputs.append(np.around(node_output[0],decimals=4))
+        if layer!='output':
+            print('the output of the nodes in hidden layer number {} is {}'.format(layer.split('_')[1],layer_outputs))
+        layer_inputs=layer_outputs
+    network_predict=layer_outputs
+    return network_predict
+predictions = forward_propogation(network,inputs)
+print('The predicted value by the network for the given input is {}'.format(np.around(predictions[0], decimals=4)))
